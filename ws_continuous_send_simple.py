7d995ec4c25136fceb75b977fe7a076fe4831ae0
42 Robotics <42robotics@42s-iMac.local>
2020-02-24 00:15:58 -0800

    final

---
+++ ws_continuous_send_simple.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import asyncio
+import websockets
+from gpiozero import Button
+import time
+from time import time_ns as now
+
+async def hello(websocket, path):
+    button = Button(17)
+    begin = now()
+    arr = list()
+    arr.append(begin)
+    step = 0
+    prev = 0
+    while True:
+        if (button.is_pressed) and prev == 0:
+            step += 1
+            prev = 1
+            press_time = now()
+            arr.append(press_time)
+            if len(arr) > 5:
+                arr.pop(0);
+
+            diff = press_time - arr[0]
+            print("diff", diff)
+            current_speed = (len(arr) * 60* 1000000000) / diff
+
+        if not (button.is_pressed):
+            prev = 0
+        if len(arr) < 2:
+            current_speed = 0
+        speed_str = str(current_speed)
+#        print("speed:", speed_str)
+        await websocket.send(speed_str)
+
+start_server = websockets.serve(hello, "0.0.0.0", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ ws_final_server_with_speed.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import asyncio
+import websockets
+from gpiozero import Button
+import time
+from time import time_ns as now
+
+async def hello(websocket, path):
+    button = Button(17)
+    begin = now()
+    arr = list()
+    arr.append(begin)
+    step = 0
+    prev = 0
+    speed = 0
+    pre_decay = 0
+
+    while True:
+        if (button.is_pressed) and prev == 0:
+            step += 1
+            prev = 1
+            press_time = now()
+            arr.append(press_time)
+            if len(arr) > 5:
+                arr.pop(0);
+
+            diff = arr[len(arr) -1] - arr[0]
+            current_speed = (len(arr) * 60* 1000000000) / diff
+            if step < 40:
+                print("step", step)
+                scaler = (-(1/2)**(step/5 -1) + 2)/2
+#                print("I m scaler", scaler, "for step", step,
+#                     "current_speed before:", current_speed)
+                speed = scaler * current_speed
+        if not (button.is_pressed):
+            prev = 0
+
+        if speed > 1:
+            decay = int((now() - arr[len(arr) - 1])/1000000000)
+            if decay > pre_decay:
+#                print("this is decay", decay, "prev", pre_decay)
+                scaler = ((1/2) ** (decay/2 - 1))/2
+                speed = scaler * speed
+                pre_decay = decay
+                print("this is decay", decay, "prev", pre_decay, "scaler", scaler)
+            if decay < 1:
+                pre_decay = 0
+                
+        else:
+            speed = 0
+            step = 0
+
+        speed_str = str(speed)
+        print("speed:", speed_str)
+        await websocket.send(speed_str)
+
+start_server = websockets.serve(hello, "0.0.0.0", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
